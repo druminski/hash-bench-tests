77dce01f78f43a54bf8f6c11b0d07865df81ba09
Tomasz Janiszewski <tomasz.janiszewski@allegrogroup.com>
2016-04-04 12:36:07 +0200

    Add other hash functions

--- hash_bench_test.go
+++ hash_bench_test.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"testing"
+	"hash/crc64"
 	"hash/fnv"
 	"fmt"
 	farm "github.com/dgryski/go-farm"
 )
 
-func BenchmarkFnvHash(b *testing.B) {
+func BenchmarkFnvaHash(b *testing.B) {
 	h := fnv.New64a()
 	for i := 0; i < b.N; i++ {
 		h.Write([]byte(key(i)))
 		h.Sum64()
+	}
+}
+
+func BenchmarkFnvHash(b *testing.B) {
+	h := fnv.New64()
+	for i := 0; i < b.N; i++ {
+		h.Write([]byte(key(i)))
+		h.Sum64()
+	}
+}
+
+func BenchmarkCRCHash(b *testing.B) {
+	table := crc64.MakeTable(crc64.ISO)
+	for i := 0; i < b.N; i++ {
+		crc64.Checksum([]byte(key(i)), table)
 	}
 }
 
